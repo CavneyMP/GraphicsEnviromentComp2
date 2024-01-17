8b0b22ab94c8bfe63a9877cc0bacee6f9aed0181
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-01-17 17:25:20 +0000

    UserDefinedMethod Tests

---
+++ GraphicsEnviromentComp2Tests/Commands/UserDefinedMethodTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphicsEnvironmentComp2;
+using System.Collections.Generic;
+using GraphicsEnvironmentComp2.Commands;
+using static GraphicsEnvironmentComp2.Form1;
+
+namespace GraphicsEnvironmentComp2.Tests
+
+{
+    /// <summary>
+    /// This class contains unit tests for the user defined methods, to ensure it works properly
+    /// </summary>
+    [TestClass()]
+    public class UserDefinedMethodTests
+    {
+        /// <summary>
+        /// Tests whether the constructor initializes the properties correctly
+        /// </summary>
+        [TestMethod()]
+        public void UserDefinedMethod_Constructor_InitializesProperties()
+        {
+            string methodName = "MyMethod";
+            List<string> parameters = new List<string> { "param1", "param2" };
+            List<ICommandInterface> commands = new List<ICommandInterface>
+            {
+                new SimulateCommand(),
+                new SimulateCommand()
+            };
+
+            var userDefinedMethod = new UserDefinedMethod(methodName, parameters, commands);
+
+           
+            Assert.AreEqual(methodName, userDefinedMethod.MethodName);
+            CollectionAssert.AreEqual(parameters, userDefinedMethod.Parameters);
+            CollectionAssert.AreEqual(commands, userDefinedMethod.Commands);
+        }
+
+        /// <summary>
+        /// A mock implementation of the iCommand interface for testing purposes.
+        /// </summary>
+
+        private class SimulateCommand : ICommandInterface
+        {
+            public void Execute(SafeGraphics safeGraphics)
+            {
+                // Simulate Execute method
+            }
+        }
+    }
+}
