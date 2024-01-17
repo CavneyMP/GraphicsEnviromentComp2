fd24d880a711d96a5290d2f9aa75aa8eb7cc2ea6
CavneyMP01 <matthewcavney@yahoo.co.uk>
2024-01-17 17:33:11 +0000

    EndIF Test and XML

---
+++ GraphicsEnviromentComp2Tests/Commands/EndIfCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphicsEnvironmentComp2.Commands;
+using System.Drawing;
+
+namespace Tests
+{
+
+    /// <summary>
+    /// Unit tests to test the endIf command
+    /// </summary>
+    [TestClass()]
+    public class EndIfCommandTests
+    {
+    /// <summary>
+    /// Tests that the endif command execute method resets the flag. 
+    /// </summary>
+        [TestMethod()]
+        public void Execute_ShouldResetShouldExecuteNextFlag()
+        {
+            // reset flag
+            IfCommand.ResetExecutionFlag();
+
+            IfCommand.SetConditionResult(false);
+
+            var endIfCommand = new EndIfCommand();
+
+            endIfCommand.Execute(null);
+
+            // Assert that the ExecuteNext flag is reset to true
+            Assert.IsTrue(IfCommand.ExecuteNext, "EndIfCommand should reset the ExecuteNext flag to true.");
+        }
+    }
+}
